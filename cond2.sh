a284d1d8b36bd69ea295918c24306ed96d2744a9
ekamleen <ekamleenm@gmail.com>
2024-09-15 23:42:02 -0700

    bash

--- cond2.sh
+++ cond2.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+if [ -x $1 ]
+	then
+		x_status="true"
+	else
+		x_status="false"
+	fi
+
+
+if [ -w $1 ]
+	then
+		w_status="true"
+	else
+		w_status="false"
+	fi
+
+echo $x_status, $w_status
+
